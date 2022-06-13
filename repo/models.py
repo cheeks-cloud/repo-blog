5b161bf1e438a5d4ac0d57294c6716dc9c28c9b9
@cheeks cloud <agneskaswii700@gmail.com>
2022-06-13 09:01:37 +0300

    adding Review Model

--- repo/models.py
+++ repo/models.py
@@ -1,3 +1,35 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+
+
+
+
+class Profile(models.Model):   #add this class and the following fields
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	projects = models.ManyToManyField(Projects)
+
+
+class Review(models.Model):
+    CHOICES1 = [
+        ('Excellent', 'Excellent'),
+        ('Good', 'Good'),
+        ('Average', 'Average'),
+        ('Not Good', 'Not Good'),      
+    ]
+    design= models.CharField( choices=CHOICES1, max_length=30)
+
+    CHOICES = [
+        ('Excellent', 'Excellent'),
+        ('Good', 'Good'),
+        ('Average', 'Average'),
+        ('Not Good', 'Not Good'),      
+    ]
+    usability = models.CharField( choices=CHOICES, max_length=30)
+    project_on_review = models.ForeignKey(Projects,on_delete=models.CASCADE)
+    content = models.TextField()
+
+    def __str__(self):
+      return self.content
