96783bc67c47a157b6df7e50569c8ac7df9feaa9
@cheeks cloud <agneskaswii700@gmail.com>
2022-06-14 00:17:42 +0300

    fixing styling

--- project/settings.py
+++ project/settings.py
@@ -3,8 +3,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-g^-vu8mam^lnju^kd8s&5ccesehvca*g^&_qg4f(vnzvks_-x4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -106,8 +106,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static/')
-
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
