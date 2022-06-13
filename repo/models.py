4c70f25319b395fe4ccbe05d73386e8bea34546a
@cheeks cloud <agneskaswii700@gmail.com>
2022-06-13 09:18:59 +0300

    adding Profile methods

--- repo/models.py
+++ repo/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save 
 # Create your models here.
 
 class Projects(models.Model):
@@ -21,7 +23,17 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField()
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
-    projects = models.ManyToManyField(Projects)
+    projects = models.ForeignKey(Projects, on_delete=models.CASCADE)
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+          Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User) 
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
 
 class Review(models.Model):
     CHOICES1 = [
