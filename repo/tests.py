8dfaafb6efe3efe530c1c5e62515966df8daa0a7
@cheeks cloud <agneskaswii700@gmail.com>
2022-06-14 03:36:03 +0300

    adding tests

--- repo/tests.py
+++ repo/tests.py
@@ -1,7 +1,7 @@
 
 from django.test import TestCase
 from django.contrib.auth.models import User
-from .models import Profile
+from .models import Profile,Review
 import datetime
 
 # Create your tests here.
